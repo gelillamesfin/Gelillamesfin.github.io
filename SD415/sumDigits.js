440d978c2aab4bf8466d3b7d6584c3e25354ffa8
Gelilla Tafa <144738155+gelillamesfin@users.noreply.github.com>
2023-11-06 16:05:52 -0600

    Delete SD415 directory

--- SD415/sumDigits.js
+++
@@ -1,9 +0,0 @@
-function sumDigits(number) {
-  let sum = 0;
-  while (number) {
-    sum += number % 10;
-    number = Math.floor(number / 10);
-  }
-  return sum;
-}
-console.log(sumDigits(1234));
