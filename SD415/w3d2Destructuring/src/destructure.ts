234e86808f587662347f2c30bb56d924a903c76d
Gelilla Tafa <144738155+gelillamesfin@users.noreply.github.com>
2023-11-14 21:43:19 -0600

    Update destructure.ts

--- SD415/w3d2Destructuring/src/destructure.ts
+++ SD415/w3d2Destructuring/src/destructure.ts
@@ -7,13 +7,36 @@ P.S. Use Object.entries and destructuring to iterate over key/value pairs.
 
 // the following type definition says that SalaryObj has keys of type string and values of type number
 type SalaryObj = { [key: string]: number };
+ export function topSalary (salaries:SalaryObj):string|null{
+
+    let topSalary=Object.values(salaries)[0];
+    let topPaid=Object.keys(salaries)[0]
+// console.log(topPaid)
+// console.log(topSalary)
+
+  
+const allthesalaries=Object.values(salaries);
 
 
+for (const salary of allthesalaries){
+    if (salary> topSalary){
+        topSalary=salary
+    }
+   for(const [key,value] of Object.entries(salaries)){
+    if (value> topSalary){
+        topSalary=value
+        topPaid=key
+    }
+   }
 
-
-
- export function  topSalary( obj:SalaryObj ):string {
-let topPaid:string 
- 
-return topPaid
 }
+if (topPaid!==null){
+    return (`Top Paid is ${topPaid}`);
+}else {
+    return ("no top salary")
+}
+}
+
+// return topPaid;
+
+//console.log (topSalary(salaries2));
